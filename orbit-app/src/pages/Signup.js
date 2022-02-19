1747485e11ebd96d00fa2739b1e5e0f644f9a462
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-19 10:32:00 +0000

    Integrate With Signup

--- orbit-app/src/pages/Signup.js
+++ orbit-app/src/pages/Signup.js
@@ -10,6 +10,8 @@ import GradientBar from './../components/common/GradientBar';
 import FormError from './../components/FormError';
 import FormSuccess from './../components/FormSuccess';
 import logo from './../images/logo.png';
+import {publicFetch} from "../util/fetch";
+import {Redirect} from "react-router";
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string().required(
@@ -26,10 +28,21 @@ const Signup = () => {
   const [signupSuccess, setSignupSuccess] = useState();
   const [signupError, setSignupError] = useState();
   const [loginLoading, setLoginLoading] = useState(false);
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false);
 
   const submitCredentials = async credentials => {
     try {
       setLoginLoading(true);
+      const {data} = await publicFetch.post(
+          'signup',
+          credentials
+      )
+      setSignupSuccess(data.message)
+      setSignupError('')
+      setTimeout(() => {
+        setRedirectOnLogin(true)
+      }, 700)
+
     } catch (error) {
       setLoginLoading(false);
       const { data } = error.response;
@@ -40,6 +53,7 @@ const Signup = () => {
 
   return (
     <>
+      {redirectOnLogin && <Redirect to="/dashboard" />}
       <section className="w-1/2 h-screen m-auto p-8 sm:pt-10">
         <GradientBar />
         <Card>
